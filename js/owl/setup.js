995e7a44e3cc26e187f00db6fec8285fb7ea1dc5
RB-Amanda <romero.amd01@gmail.com>
2021-04-21 19:39:12 -0300

    commit inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function redirecionar1(){
+    window.open("https://www.youtube.com/watch?v=RuiklpUQ-p4")
+
+}
+
+function redirecionar2(){
+    window.open("https://www.imdb.com/title/tt2243973/?ref_=fn_al_tt_1")
+}
